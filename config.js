38571146e491021210eebed4a08031d04d1f9720
Jesse Bakker <jesse.bakker@kadaster.nl>
2023-01-20 11:46:15 +0100

    Add abstract and latest version url

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 // All config options at https://respec.org/docs/
 var respecConfig = {
     // hack to skip W3C specStatus validation
-    latestVersion: "",
+    latestVersion: "https://bp4mc2.github.io/algoritmes/",
     // Working Groups ids at https://respec.org/w3c/groups/
     group: "webapps",
     xref: "web-platform",
     specStatus: "unofficial",
-    maxTocLevel: 5,
     editors: [
         {
             name: "Arjen Santema"
